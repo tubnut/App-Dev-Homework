2322416466210eea7989aa6f4619ad01441d9c08
tubnut <74440886+tubnut@users.noreply.github.com>
2023-01-30 19:28:17 -0600

    Need to finish ace values, dealer conditionals, and draw functions

--- Lab2B_McGriff/BlackJackRunner.py
+++ Lab2B_McGriff/BlackJackRunner.py
@@ -8,11 +8,18 @@ def dealCards():
     for c in range(2):
         playerHand.append(deck.draw())
 
-def countPoints():
-    pass
+def countPoints(hand):
+    output = []
+    for card in hand:
+        output.append(card.value)
+    return sum(output)
 
 def startsWithTen():
     pass
+
+def displayHand(player):
+    for card in playerHand:
+        print(card)
 
 deck = BlackJackDeck()
 
@@ -21,9 +28,7 @@ dealerPoints = 0
 playerHand = []
 playerPoints = 0
 
-
-
-startLoop = input("Would you like to play a game?(y/n) ")
+startLoop = input(Fore.WHITE + "Would you like to play a game?(y/n) ")
 while startLoop == 'y':
     if startLoop == "n":
         quit()
@@ -33,12 +38,10 @@ while startLoop == 'y':
     print('\n' + Fore.RED + f"Dealer shows: {dealerHand[0]} \n")
 
     print(Fore.GREEN + "Player has:")
-    print(Fore.GREEN + '\n'.join(playerHand))
-    print(Fore.GREEN + f"\nCurrent player points: {}")
+    displayHand(playerHand)
 
+    print("Player points: " + str(countPoints(playerHand)))
 
-    ask = input("Would you like to draw a card(y/n)")
+    ask = input(Fore.WHITE + "Would you like to draw a card(y/n) ")
     if ask == 'n':
         startLoop = 'n'
-    
-
