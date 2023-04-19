e361b8ded3640694a12a6a3a715799c1dacdd7f1
tubnut <74440886+tubnut@users.noreply.github.com>
2023-04-18 20:01:27 -0500

    almost

--- Maze Game/Player.py
+++ Maze Game/Player.py
@@ -10,25 +10,30 @@ class Player():
         self.color = color
         self.img = pg.image.load('Resources/goofyAhhCat.png')
         self.rect = self.img.get_rect()
+    
+    def __str__(self):
+        return f"({self.x_pos}, {self.y_pos})"
 
     def draw(self, window):
         window.blit(self.img,(self.x_pos,self.y_pos))    
 
     def move_up(self):
-        self.y_pos -= 10
+        self.y_pos -= 0.5
 
     def move_down(self):
-        self.y_pos += 10
+        self.y_pos += 0.5
     
     def move_left(self):
-        self.x_pos -= 10
+        self.x_pos -= 0.5
     
     def move_right(self):
-        self.x_pos += 10
+        self.x_pos += 0.5
 
     def collision(self, other):
-        if (self.y_pos + 50 >= other.y_pos and self.y_pos <= other.y_pos + 50) and (self.x_pos + 50 >= other.x_pos and self.x_pos <= other.x_pos + 50):
+        if (self.y_pos + 30 >= other.y_pos and self.y_pos <= other.y_pos + 30) and (self.x_pos + 30 >= other.x_pos and self.x_pos <= other.x_pos + 30):
             return True
 
-    
-    
+    def setPos(self, x, y):
+        self.x_pos = x
+        self.y_pos = y
+
--- Maze Game/Wall.py
+++ Maze Game/Wall.py
@@ -11,9 +11,9 @@ class Wall():
         self.img = pg.image.load('Resources/wall.png')
         self.rect = self.img.get_rect()
 
-
     def draw(self, window):
         window.blit(self.img,(self.x_pos,self.y_pos))
 
-    
-    
+    def collision(self, other):
+        if (self.y_pos + 50 >= other.y_pos and self.y_pos <= other.y_pos + 50) and (self.x_pos + 50 >= other.x_pos and self.x_pos <= other.x_pos + 50):
+            return True
