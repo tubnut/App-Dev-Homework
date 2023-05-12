691ffb45f5e5242909af929accbff4ad939ffec1
tubnut <74440886+tubnut@users.noreply.github.com>
2023-05-11 22:39:51 -0500

    board jamer

--- Board Game Final/Checkers.py
+++ Board Game Final/Checkers.py
@@ -1,13 +1,18 @@
 import pygame
 from pygame.locals import *
 
-class Checkers():
+class Checkers(pygame.sprite.Sprite):
 
     def __init__(self, x, y, color) -> None:
+        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.color = color
-        self.image = pygame.image.load("Resources/blackPiece") if color == "black" else pygame.image.load("Resources/redPiece")
+        self.image = pygame.image.load("Resources/blackPiece.png") if color == "black" else pygame.image.load("Resources/redPiece.png")
+        self.rect = self.image.get_rect()
     
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y})"
+
     def draw(self, window):
-        pass
+        window.blit(self.image, self.rect)
--- Board Game Final/main.py
+++ Board Game Final/main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from Board import Board
+from Checkers import Checkers
 
 pygame.init()
 
@@ -10,6 +11,16 @@ screen = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 
 board = Board()
+redPieces = pygame.sprite.Group()
+blackPieces = pygame.sprite.Group()
+
+for _ in range(1,12):
+    r = Checkers(0,0, "red")
+    redPieces.add(r)
+
+for _ in range(1,12):
+    b = Checkers(0,0, "black")
+    blackPieces.add(r)
 
 running = True
 while running:
@@ -21,6 +32,9 @@ while running:
     screen.fill("black")
 
     board.draw(screen)
+    redPieces.draw(screen)
+
+
 
     pygame.display.flip()
     clock.tick(FPS)
